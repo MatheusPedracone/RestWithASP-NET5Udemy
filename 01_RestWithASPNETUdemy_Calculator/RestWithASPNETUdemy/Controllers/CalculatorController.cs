439fd8786fb708a50ce6b4f060d22e01eb152c8b
Matheus Augusto <matheuspedracone@gmail.com>
2021-09-29 17:38:25 -0300

    Implementing calculator

---
+++ 01_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+        private bool IsNumeric(string strNumber)
+        {
+           double number;
+           bool isNumber = double.TryParse(
+               strNumber,System.Globalization.NumberStyles.Any, 
+               System.Globalization.NumberFormatInfo.InvariantInfo,
+                out number);
+
+           return isNumber;
+        }
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(strNumber, out decimalValue))
+            {
+               return decimalValue; 
+            }
+             return 0;
+        }
+    }
+}
