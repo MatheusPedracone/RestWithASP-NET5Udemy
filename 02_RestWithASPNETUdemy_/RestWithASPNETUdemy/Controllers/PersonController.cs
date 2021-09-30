6ed01ef6d91e4f9862fbed89571b0f558580105f
Matheus Augusto <matheuspedracone@gmail.com>
2021-09-30 09:06:33 -0300

    Creating with four verbs

---
+++ 02_RestWithASPNETUdemy_/RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult Get()
+        {
+            return BadRequest("Invalid input");
+        }
+     }
+}
+
